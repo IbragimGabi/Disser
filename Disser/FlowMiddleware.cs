2f0458dafed920f506720cd7a246ffbec0ec79ab
Ибрагим Габидуллин <ibragim0795@mail.ru>
2018-04-09 21:40:05 +0300

    proof of concept with test

---
+++ Disser/FlowMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Disser
+{
+    public class FlowMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public FlowMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Query["token"];
+            if (token != "12345678")
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Token is invalid");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next.Invoke(context);
+
+        }
+
+    }
+}
